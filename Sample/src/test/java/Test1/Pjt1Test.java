8e8c51c2865f715b2987cee3a158534a0b5c92b2
GauthamRamesh <gautham.r92@gmail.com>
2024-03-26 13:06:16 +0530

    2

---
+++ Sample/src/test/java/Test1/Pjt1Test.java
@@ -0,0 +1,13 @@
+package Test1;
+
+import org.testng.annotations.Test;
+
+public class Pjt1Test {
+
+	@Test
+	public void m1() {
+		
+		System.out.println("hello");
+	}
+
+}
---
+++ Sample/src/test/java/Test1/Pjt2Test.java
@@ -0,0 +1,12 @@
+package Test1;
+
+import org.testng.annotations.Test;
+
+public class Pjt2Test {
+
+	@Test
+	public void m2(){
+		System.out.println("hi");
+	}
+
+}
